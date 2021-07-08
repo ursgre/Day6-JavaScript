e49b3392b61da687f0a53cea47592d254e770108
ursgre <urszulagrezicka@gmail.com>
2021-07-08 15:30:59 +0200

    Initial commit

---
+++ JavaScript1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Pierwszy teskt</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Drugi tekst"'>Kliknij tu!</button>
+
+</body>
+</html>
